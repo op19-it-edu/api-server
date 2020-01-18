1a696ab556d771ab8e0247bf5ae0d08a34274b02
keita.noguchi <ktngch613@gmail.com>
2020-01-18 23:14:04 +0900

    Add entities

--- model/entity.go
+++ model/entity.go
@@ -1,16 +1,45 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 // 構造体などを定義する
 
-// usersテーブルの定義
+// usersテーブル
 type User struct {
 	// gorm.ModelはID, CreatedAt, UpdatedAt, DeletedAtを自動挿入する
 	gorm.Model
 	// タグでDBに作成されるカラム名やデータ型を指定する
 	Name        string `gorm:"type:text;column:user_name"`
-	Email       string `gorm:"type:text;column:user_password"`
-	Password    string `gorm:"type:text;column:user_email"`
+	Password    string `gorm:"type:text;column:user_password"`
 	Discription string `gorm:"type:text;column:user_discription"`
+
+	// has manyの関係を明示
+	Relations  []Relation
+	TodoTweets []TodoTweet
+}
+
+// relationsテーブル
+type Relation struct {
+	gorm.Model
+
+	// 外部キー（usersテーブル）
+	UserID   uint
+	FollowID uint
+}
+
+// todo_tweetsテーブル
+type TodoTweet struct {
+	gorm.Model
+
+	// 外部キー（usersテーブル）
+	UserID uint
+
+	// typeでデータ型を指定
+	Todo     string    `gorm:"type:text"`
+	Deadline time.Time `gorm:"type:datetime"`
+	Done     bool      `gorm:"type:boolean"`
 }
